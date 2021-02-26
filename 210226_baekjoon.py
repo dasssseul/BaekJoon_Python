41cd3d52ee65ee1cfe254204c0b68199e3aa2c2b
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-02-26 22:04:56 +0900

    Add files via upload

---
+++ 210226_baekjoon.py
@@ -0,0 +1,36 @@
+# 1408_내일 다시 풀께용
+
+# 2741
+n = int(input())
+for i in range(1,n+1):
+    print(i)
+
+# 2742
+n = int(input())
+for i in range(n,0,-1):
+    print(i)
+
+# 2739
+n = int(input())
+for i in range(1,10):
+    print(n,'*',i,'=',n*i)
+
+# 2438
+n = int(input())
+for i in range(1,n+1):
+    print('*'* i)
+
+# 2439
+n = int(input())
+for i in range(1,n+1):
+    print(' '*(n-i)+'*'* i)
+
+# 2440
+n = int(input())
+for i in range(n,0,-1):
+    print('*'* i)
+
+# 2441
+n = int(input())
+for i in range(n,0,-1):
+    print(' '*(n-i)+'*'* i)
