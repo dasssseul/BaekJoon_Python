2300f96397d2228f2272abe1425509765d7ee8f5
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-03-27 22:47:35 +0900

    Add files via upload

---
+++ algorithm/210327_baekjoon.py
@@ -0,0 +1,23 @@
+# 기본수학1
+
+#2869_달팽이는 올라가고싶다_시간초과
+import sys
+a, b, v = map(int, sys.stdin.readline().split())
+length = day = 0
+while True:
+    day += 1
+    length += a
+    if length >= v:
+        print(day)
+        break
+    else:
+        length -= b
+
+#2869_달팽이는 올라가고싶다_모범답안
+import sys
+a, b, v = map(int, sys.stdin.readline().split())
+high = (v-b) / (a-b) #정상에 도달하면 밤에는 미끄러지지 않기 때문에 분자에 b 빼주기
+if float(high) > int(high): #소수점이 생기는 경우는 다음날로 생각
+    print(int(high)+1)
+else:
+    print(int(high))
