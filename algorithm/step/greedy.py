1cadb8d5496bf735f1671ca0ef42f99e69545439
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-04-08 11:33:03 +0900

    Update greedy.py

--- algorithm/step/greedy.py
+++ algorithm/step/greedy.py
@@ -1,7 +1,9 @@
 
-#greedy 알고리즘
+# 단계별로 풀기
+# greedy 알고리즘
 
-#11399번_ATM
+
+# 11399번_ATM
 
 n = int(input())
 p = list(map(int, input().split()))
@@ -9,6 +11,24 @@ p = sorted(p)
 for i in range(1,n):
     p[i] = p[i-1]+p[i]
 print(sum(p))
+
+
+# 13305번_주유소
+
+n = int(input())
+dist = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+result = dist[0]*cost[0]
+min_cost = cost[0]
+
+for i in range(1,len(dist)):
+    if cost[i] >= min_cost:
+        result += min_cost * dist[i]
+    else:
+        min_cost = cost[i]
+        result += min_cost * dist[i]
+
+print(result)
 
 
 
