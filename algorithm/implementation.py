d10475b900ba90ba044aa9ec15181f37eb668ed1
dasssseul <kds0339@naver.com>
2021-04-15 17:24:09 +0900

    1547번 공 문제 풀이 추가

--- algorithm/implementation.py
+++ algorithm/implementation.py
@@ -86,5 +86,17 @@ while True:
     i = (i+l)%n
     result += 1
     
-    
-    
+
+# 1547번. 공
+
+m = int(input())
+
+ball = [0, 1, 0, 0]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    ball[x], ball[y] = ball[y], ball[x]
+
+for i in ball:
+    if i == 1:
+        print(ball.index(i))
