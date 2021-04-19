c684475822330f2f07078625874a9f795e238ef9
dasssseul <kds0339@naver.com>
2021-04-19 12:20:35 +0900

    update 1260

---
+++ algorithm/dfs, bfs.py
@@ -0,0 +1,37 @@
+
+# 백준
+# 1260번. DFS와 BFS
+
+from collections import deque
+
+n, m, v = map(int, input().split())
+visited = [0] * (n+1)
+node = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(m):
+    x, y = map(int, input().split())
+    node[x][y] = node[y][x] = 1
+
+def dfs(v):
+    visited[v] = 1
+    print(v, end=' ')
+    for i in range(1, n+1):
+        if visited[i] == 0 and node[v][i] == 1:
+            dfs(i)
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in range(1, n+1):
+            if visited[i] == 0 and node[v][i] == 1:
+                queue.append(i)
+                visited[i] = 1
+
+dfs(v)
+print()
+visited = [0] * (n+1)
+bfs(v)
+
