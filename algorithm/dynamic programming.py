3a9ff027d9df26e414ccf0fc60e6a3eac806dcec
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-05-04 16:00:38 +0900

    Update dynamic programming.py

--- algorithm/dynamic programming.py
+++ algorithm/dynamic programming.py
@@ -66,4 +66,22 @@ print(max(dp))
 
 
 
+# 1932번. 정수 삼각형
+
+n = int(input())
+semo = [list(map(int,input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            semo[i][j] += semo[i-1][j]
+        elif j == i:
+            semo[i][j] += semo[i-1][j-1]
+        else:
+            semo[i][j] += max(semo[i-1][j-1], semo[i-1][j])
+
+print(max(semo[n-1]))
+
+
+
 
