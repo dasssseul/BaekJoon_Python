5552bba7723dd77d0a3945cbcddfd12d3b2cc6f1
dasssseul <kds0339@naver.com>
2021-05-27 20:05:48 +0900

    update math

---
+++ algorithm/math.py
@@ -0,0 +1,16 @@
+
+# 백준
+
+# 11050번. 이항계수1
+
+from itertools import combinations
+
+n, k = map(int, input().split())
+n_list = []
+
+for i in range(n):
+    n_list.append(i)
+
+result = combinations(n_list, k)
+print(len(list(result)))
+
