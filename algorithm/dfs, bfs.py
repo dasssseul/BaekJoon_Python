ea816fe03e95654dc0f7c05b6b8c40603d1ce07b
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-05-30 22:46:12 +0900

    Update dfs, bfs.py

--- algorithm/dfs, bfs.py
+++ algorithm/dfs, bfs.py
@@ -38,3 +38,41 @@ bfs(v)
 
 
 
+
+# 4963번. 섬의 개수
+
+def island_dfs(x, y):
+    if x <= -1 or x >= h or y <= -1 or y >= w:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 2
+        island_dfs(x-1, y)
+        island_dfs(x-1, y-1)
+        island_dfs(x, y-1)
+        island_dfs(x+1, y-1)
+        island_dfs(x+1, y)
+        island_dfs(x+1, y+1)
+        island_dfs(x, y+1)
+        island_dfs(x-1, y+1)
+        return True
+    return False
+
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    graph = []
+    result = 0
+    for i in range(h):
+        graph.append(list(map(int,input().split())))
+    for j in range(h):
+        for k in range(w):
+            if island_dfs(j, k) == True:
+                result += 1
+    print(result)
+
+
+    
+
+
