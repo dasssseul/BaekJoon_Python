a933b0c3a95cd96aa80286e6b37afd7c8dd8a778
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-05-31 12:46:42 +0900

    Update dfs, bfs.py

--- algorithm/dfs, bfs.py
+++ algorithm/dfs, bfs.py
@@ -74,5 +74,44 @@ while True:
 
 
     
+# 2667번. 단자 번호 붙이기
 
+n = int(input())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+def dfs(x, y):
+    global cnt
+    if x <= -1 or x >= n or y <= -1 or y >= n:
+        return False
+    if graph[x][y] == 1:
+        cnt += 1
+        graph[x][y] = 2
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+danji_num = []
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if dfs(i, j) == True:
+            result += 1
+            danji_num.append(cnt)
+            cnt = 0
+print(result)
+danji_num.sort()
+for i in danji_num:
+    print(i)
+    
+    
+    
 
